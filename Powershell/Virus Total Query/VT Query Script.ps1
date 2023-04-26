0dc26058b4ff5ecfaaa48990226c8c4924453071
Lionel Faleiro <lionelxp@gmail.com>
2023-04-26 13:03:50 +0530

    Rearranged folders and updated with VT Script

---
+++ Powershell/Virus Total Query/VT Query Script.ps1
@@ -0,0 +1,23 @@
+﻿# Developed by @sandmaxprime
+
+$headers=@{}
+$headers.Add("accept", "application/json")
+#Replace apikeyhere with your Virus Total Public or Enterprise APIKey
+$headers.Add("x-apikey", "apikeyhere")
+
+$vt_url = 'https://www.virustotal.com/api/v3/files'
+
+$csv_path = 'C:\Users\Lionel\Desktop\IOCTest.csv'
+
+$hashes = Import-Csv -Path $csv_path | Select-Object -ExpandProperty Hash
+
+write-output "Hash,MeaningfulName,Microsoft Malicious,Signature Type"
+
+foreach ($hash in $hashes) {
+
+    $query_url = "$vt_url/$hash"
+
+    $response = Invoke-RestMethod -Uri $query_url -Method GET -Headers $headers
+    write-output "$($hash),$($response.data.attributes.meaningful_name),$($response.data.attributes.last_analysis_results.Microsoft.category),$($response.data.attributes.last_analysis_results.Microsoft.result) "
+
+}
---
+++ Python/IPGeoInfo.py
@@ -0,0 +1,41 @@
+#Requires Python3
+'''
+Created by Lionel Faleiro
+'''
+
+import requests
+import os
+
+#Queries keycdn
+url = "https://tools.keycdn.com/geo.json?host="
+
+#input file needs  to  have each IP on a new line
+filename =  'iplist.txt' 
+
+#Output file will be autocreated
+finalop =  'geolist.csv'
+fw =  open(finalop,"w+")
+
+#CSV delimiter  is |
+fw.write("IP|Country|State|City|ISP\n")
+fname = open(filename)
+for line in fname:
+    ip  = line.rstrip()
+    #print(ip)
+    try:
+        r = requests.get(url=url+ip)
+        data  = r.json()
+        isp  = data['data']['geo']['isp']
+        city  = data['data']['geo']['city']
+        state  = data['data']['geo']['region_name']
+        country  =  data['data']['geo']['country_name']
+        print(str(ip)+","+str(country)+","+str(state)+","+str(city)+","+str(isp))
+        fw.write(str(ip)+"|"+str(country)+"|"+str(state)+"|"+str(city)+"|"+str(isp)+"\n")
+    except:
+        print('Cannot find ip for',ip)
+
+fname.close()
+fw.close()     
+
+
+
