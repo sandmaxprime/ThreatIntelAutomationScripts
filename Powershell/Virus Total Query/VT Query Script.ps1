43a1deb4c7323fffb1187f193ea94217f8984b5e
Lionel Faleiro <lionelxp@gmail.com>
2024-06-13 20:17:42 +0530

    Update VT Query Script.ps1
    updated with try catch block and output file

--- Powershell/Virus Total Query/VT Query Script.ps1
+++ Powershell/Virus Total Query/VT Query Script.ps1
@@ -3,11 +3,12 @@
 $headers=@{}
 $headers.Add("accept", "application/json")
 #Replace apikeyhere with your Virus Total Public or Enterprise APIKey
-$headers.Add("x-apikey", "apikeyhere")
+$headers.Add("x-apikey", "yourapikeyhere")
 
 $vt_url = 'https://www.virustotal.com/api/v3/files'
 
 $csv_path = 'C:\Users\Lionel\Desktop\IOCTest.csv'
+$output_path = 'C:\Users\Lionel\Desktop\output.csv'
 
 $hashes = Import-Csv -Path $csv_path | Select-Object -ExpandProperty Hash
 
@@ -15,9 +16,20 @@ write-output "Hash,MeaningfulName,Microsoft Malicious,Signature Type"
 
 foreach ($hash in $hashes) {
 
-    $query_url = "$vt_url/$hash"
+    try
+    {
+        $query_url = "$vt_url/$hash"
 
-    $response = Invoke-RestMethod -Uri $query_url -Method GET -Headers $headers
-    write-output "$($hash),$($response.data.attributes.meaningful_name),$($response.data.attributes.last_analysis_results.Microsoft.category),$($response.data.attributes.last_analysis_results.Microsoft.result) "
-
+        $response = Invoke-RestMethod -Uri $query_url -Method GET -Headers $headers -OutVariable GetStatusResponse
+        write-output "$($hash),$($response.data.attributes.meaningful_name),$($response.data.attributes.last_analysis_results.Microsoft.category),$($response.data.attributes.last_analysis_results.Microsoft.result) "
+        if ($response.data.attributes.last_analysis_results.Microsoft.category -ieq "undetected")
+        {
+            Write-Output "undetected hits: $($hash)" >> $output_path
+        }
+ 
+    }
+    catch 
+    {
+        Write-Output "unsuccessful hits: $($hash)" >> $output_path
+    }
 }
